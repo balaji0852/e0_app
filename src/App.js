08b8f852f13bf3ddf0a930065b2ef94582f894a8
Balaji <balajikumar189@gmail.com>
2021-02-10 08:28:10 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, lazy} from 'react';
+import './App.css'
+import FoodDetails from './FoodDetails/FoodDetails';
+import ColumnName from './FoodDetails/Pages/cells/ColumnName';
+import Tab from './FoodDetails/Tabs/Tab';
+import StatsComponent from './Stats/StatsComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component{
+
+
+  
+
+  render(){
+
+    const data = {
+      'name':'idly',
+      'last_update':'Feb 10',
+      'cogs':'10.00',
+      'cost_price':'100.00',
+      'sale_price':'120.00',
+      'gross_margin':'25.00'
+    }
+
+
+    return (<div id='main'>
+      <StatsComponent></StatsComponent>
+      <br></br>
+      <div id='Tabs'>
+        <Tab name='ALL RECIPE(S)'></Tab>
+        <Tab name='INCORRECT'></Tab>
+        <Tab name='UNTAGGED'></Tab>
+        <Tab name='DISABLED'></Tab>
+      </div>
+      <div id='Layout'>
+      <ColumnName ColumnName='True'></ColumnName>
+      <ColumnName ColumnName='False' state={data}></ColumnName>
+      <ColumnName ColumnName='False' state={data}></ColumnName>
+      <ColumnName ColumnName='False' state={data}></ColumnName>
+      <ColumnName ColumnName='False' state={data}></ColumnName>
+      <ColumnName ColumnName='False' state={data}></ColumnName></div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
